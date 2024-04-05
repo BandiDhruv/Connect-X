9c37fc2e614e242077c466b96e812867fe925321
Dhruv Bandi <dhruvbandi03@gmail.com>
2024-04-05 10:08:52 +0530

    frontend started creating

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1 className="w-full h-screen bg-red-500 flex flex-col items-center justify-center" >
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      <div>hi</div>
+      {/* Simple React Typescript Tailwind Sample */}
+    </h1>
+  );  
+}
+
+export default App;
